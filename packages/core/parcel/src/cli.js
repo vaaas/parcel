6fbcfc07373e7a1416578e928ad7d456ca48c6ac
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-09-14 14:59:57 +0200

    Don't load plugins relative to project

--- packages/core/parcel/src/cli.js
+++ packages/core/parcel/src/cli.js
@@ -231,7 +231,7 @@ async function run(
     entries,
     // $FlowFixMe[extra-arg] - flow doesn't know about the `paths` option (added in Node v8.9.0)
     defaultConfig: require.resolve('@parcel/config-default', {
-      paths: [fs.cwd(), __dirname],
+      paths: [/* fs.cwd(), */ __dirname],
     }),
     shouldPatchConsole: true,
     ...options,
