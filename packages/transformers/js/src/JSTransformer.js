be6c857f393c77af9311a685697727837d748734
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2023-07-12 09:07:06 +0200

    WIP

--- packages/transformers/js/src/JSTransformer.js
+++ packages/transformers/js/src/JSTransformer.js
@@ -919,6 +919,7 @@ export default (new Transformer({
     }
 
     asset.type = 'js';
+    // console.log(asset.filePath, compiledCode.toString())
     asset.setBuffer(compiledCode);
 
     if (map) {
